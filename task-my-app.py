7bb88b9722dc9503238b4394c66d59cce25bd486
zainab-alghaithi <zainabalghaithi@gmail.com>
2025-02-26 11:41:56 +0400

    yesterday task

---
+++ task-my-app.py
@@ -0,0 +1,9 @@
+num_row= int(input("please enter a number:")) #type the number of the rows user wants
+ 
+
+for line_num in range (1,num_row+1):
+    for i in range(1,line_num+1):
+        print(i, end="")
+        print("") 
+
+print("End of the script!!")
